a6c7a4fa93e917d673fe1f8273dbb952c2e5e161
LukaszPasich <lukas.zed81@gmail.com>
2021-03-25 07:39:31 +0000

    FEAT: Change the way of displaying the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,161 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const cardArray = [
+    {
+        name: 'grock',
+        img: 'assets/images/card-green-rock.png'
+    },
+    {
+        name: 'grock',
+        img: 'assets/images/card-green-rock.png'
+    },
+    {
+        name: 'grock',
+        img: 'assets/images/card-green-rock.png'
+    },
+
+    {
+        name: 'gpaper',
+        img: 'assets/images/card-green-paper.png'
+    },
+    {
+        name: 'gpaper',
+        img: 'assets/images/card-green-paper.png'
+    },
+    {
+        name: 'gpaper',
+        img: 'assets/images/card-green-paper.png'
+    },
+
+    {
+        name: 'gscissors',
+        img: 'assets/images/card-green-scissors.png'
+    },
+    {
+        name: 'gscissors',
+        img: 'assets/images/card-green-scissors.png'
+    },
+    {
+        name: 'gscissors',
+        img: 'assets/images/card-green-scissors.png'
+    },
+
+    {
+        name: 'gx',
+        img: 'assets/images/card-green-x.png'
+    },
+
+    {
+        name: 'rrock',
+        img: 'assets/images/card-red-rock.png'
+    },
+    {
+        name: 'rrock',
+        img: 'assets/images/card-red-rock.png'
+    },
+    {
+        name: 'rrock',
+        img: 'assets/images/card-red-rock.png'
+    },
+
+    {
+        name: 'rpaper',
+        img: 'assets/images/card-red-paper.png'
+    },
+    {
+        name: 'rpaper',
+        img: 'assets/images/card-red-paper.png'
+    },
+    {
+        name: 'rpaper',
+        img: 'assets/images/card-red-paper.png'
+    },
+
+    {
+        name: 'rscissors',
+        img: 'assets/images/card-red-scissors.png'
+    },
+    {
+        name: 'rscissors',
+        img: 'assets/images/card-red-scissors.png'
+    },
+    {
+        name: 'rscissors',
+        img: 'assets/images/card-red-scissors.png'
+    },
+
+    {
+        name: 'rx',
+        img: 'assets/images/card-red-x.png'
+    }
+];
+
+const grid = document.querySelector('.grid');
+
+    // display cards
+
+function displayCards() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img');
+        card.setAttribute('src', 'assets/images/card-back.png');
+        card.setAttribute('data-id', i);
+        card.classList.add('js-card');
+        //card.addEventListener('click', flipCard);
+        grid.appendChild(card);
+    }
+}
+
+displayCards();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 console.log("hello js");
 
 // 1. Start the Game //
@@ -35,4 +193,3 @@ for (let i = 0; i < cards.length; i++) {
 // 8a. Winning Message //
 
 // 8b. Mark Game Status > Switch Active Player //
-
