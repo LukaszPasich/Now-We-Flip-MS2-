0ccc37d33fc6aed447fc81516cddd9d353cca256
LukaszPasich <lukas.zed81@gmail.com>
2021-03-25 22:57:32 +0000

    FEAT: Set card comparison conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,139 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 const cardArray = [
+    // green cards
     {
-        name: 'grock',
+        colour: 'green',
+        name: 'rock',
         img: 'assets/images/card-green-rock.png'
     },
     {
-        name: 'grock',
+        colour: 'green',
+        name: 'rock',
         img: 'assets/images/card-green-rock.png'
     },
     {
-        name: 'grock',
+        colour: 'green',
+        name: 'rock',
         img: 'assets/images/card-green-rock.png'
     },
 
     {
-        name: 'gpaper',
+        colour: 'green',
+        name: 'paper',
         img: 'assets/images/card-green-paper.png'
     },
     {
-        name: 'gpaper',
+        colour: 'green',
+        name: 'paper',
         img: 'assets/images/card-green-paper.png'
     },
     {
-        name: 'gpaper',
+        colour: 'green',
+        name: 'paper',
         img: 'assets/images/card-green-paper.png'
     },
 
     {
-        name: 'gscissors',
+        colour: 'green',
+        name: 'scissors',
         img: 'assets/images/card-green-scissors.png'
     },
-    {
-        name: 'gscissors',
+     {
+        colour: 'green',
+        name: 'scissors',
         img: 'assets/images/card-green-scissors.png'
     },
-    {
-        name: 'gscissors',
+     {
+        colour: 'green',
+        name: 'scissors',
         img: 'assets/images/card-green-scissors.png'
     },
 
-    {
-        name: 'gx',
+     {
+        colour: 'green',
+        name: 'x',
         img: 'assets/images/card-green-x.png'
     },
 
+    // red cards
+
     {
-        name: 'rrock',
+        colour: 'red',
+        name: 'rock',
         img: 'assets/images/card-red-rock.png'
     },
     {
-        name: 'rrock',
+        colour: 'red',
+        name: 'rock',
         img: 'assets/images/card-red-rock.png'
     },
     {
-        name: 'rrock',
+        colour: 'red',
+        name: 'rock',
         img: 'assets/images/card-red-rock.png'
     },
 
     {
-        name: 'rpaper',
+        colour: 'red',
+        name: 'paper',
         img: 'assets/images/card-red-paper.png'
     },
     {
-        name: 'rpaper',
+        colour: 'red',
+        name: 'paper',
         img: 'assets/images/card-red-paper.png'
     },
     {
-        name: 'rpaper',
+        colour: 'red',
+        name: 'paper',
         img: 'assets/images/card-red-paper.png'
     },
 
     {
-        name: 'rscissors',
+        colour: 'red',
+        name: 'scissors',
         img: 'assets/images/card-red-scissors.png'
     },
-    {
-        name: 'rscissors',
+     {
+        colour: 'red',
+        name: 'scissors',
         img: 'assets/images/card-red-scissors.png'
     },
-    {
-        name: 'rscissors',
+     {
+        colour: 'red',
+        name: 'scissors',
         img: 'assets/images/card-red-scissors.png'
     },
 
-    {
-        name: 'rx',
+     {
+        colour: 'red',
+        name: 'x',
         img: 'assets/images/card-red-x.png'
-    }
+    },
 ];
 
-let grid = document.querySelector('.grid');
-let cardsSelected =[];
+    // ----------------- Shuffle Cards -----------------
+
+    cardArray.sort(() => 0.5 - Math.random());
+
+    // ----------------- Variables -----------------
+
+const grid = document.querySelector('.grid');
+
+let cardsSelectedName =[];
+let cardsSelectedColour = [];
 let cardsSelectedId = [];
 
 
-    // ----------------- Display Cards (shuffle, plug event listeners, display cards) -----------------
+let firstCardColour = [];
+let firstCardName = [];
+
+let secondCardColour = [];
+let secondCardName = [];
+
+
+    // ----------------- Display Cards (plug event listeners, display cards) -----------------
 
 function displayCards() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -106,28 +144,93 @@ function displayCards() {
         card.addEventListener('click', selectCard);
         grid.appendChild(card);
     }
-}
+};
 
     // ----------------- Select Cards -----------------
 
-    function selectCard() {
-        var cardId = this.getAttribute('data-id');
-        cardsSelected.push(cardArray[cardId].name);
-        cardsSelectedId.push(cardId);
-        this.setAttribute('src', cardArray[cardId].img);
-        if (cardsChosen.length === 2) {
-            setTimeout(compareCards, 1000)
-        }
+function selectCard() {
+    var cardId = this.getAttribute('data-id');
+    cardsSelectedName.push(cardArray[cardId].name);
+    cardsSelectedColour.push(cardArray[cardId].colour);
+    cardsSelectedId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+        if (cardsSelectedName.length === 2) {
+        setTimeout(compareCards, 1000);
+
+        // need to add removeEventListener here
+    }
 }
+
+// ----------------- Compare Cards -----------------
+
+function compareCards() {
+    console.log(cardsSelectedName);
+    console.log(cardsSelectedColour);
+    console.log(cardsSelectedId);
+
+    firstCardColour.push(cardsSelectedColour[0]);
+    firstCardName.push(cardsSelectedName[0]);
+
+    secondCardColour.push(cardsSelectedColour[1]);
+    secondCardName.push(cardsSelectedName[1]);
+
+    console.log(firstCardColour);
+    console.log(firstCardName);
+    console.log(secondCardColour);
+    console.log(secondCardName);
+
+    // comparing cards draw condition
+    if (firstCardColour[0] === secondCardColour[0] ||
+        firstCardName[0] === secondCardName[0] ||
+        firstCardName[0] === 'x' ||
+        secondCardName[0] === 'x') {
+            console.log('draw');
+        } else if (firstCardName[0] === 'rock' && secondCardName[0] === 'paper' ||
+            firstCardName[0] === 'paper' && secondCardName[0] === 'rock') {
+            console.log('paper beats rock');
+        } else if (firstCardName[0] === 'rock' && secondCardName[0] === 'scissors' ||
+            firstCardName[0] === 'scissors' && secondCardName[0] === 'rock') {
+            console.log('rock beats scissors');
+        } else if (firstCardName[0] === 'scissors' && secondCardName[0] === 'paper' ||
+            firstCardName[0] === 'paper' && secondCardName[0] === 'scissors') {
+            console.log('scissors beat paper');
+        } else {
+        console.log('error');
+        }
+
+    
+    
+    firstCardColour = [];
+    firstCardName = [];
+
+    secondCardColour = [];
+    secondCardName = [];
+    
+    cardsSelected =[];
+    cardsSelectedColour = [];
+    cardsSelectedId = [];
+
+};
+
+
+
+// ----------------- Outcome Draw -----------------
+
+/*
+function outcomeDraw() {
+    console.log('draw');
+}
+*/
+
 
 
 displayCards();
 
+console.log(cardArray);
+
 
 
 });
-
-
 
 
 
@@ -174,3 +277,5 @@ for (let i = 0; i < cards.length; i++) {
 // 8a. Winning Message //
 
 // 8b. Mark Game Status > Switch Active Player //
+
+
